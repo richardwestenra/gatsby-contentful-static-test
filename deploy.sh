86c2e70e22da2f54b6177ee298c00ffd08b7d7fd
Richard Westenra <rjwestenra@gmail.com>
2017-08-30 11:52:41 +0100

    Configure Contentful and CircleCI

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+# via https://tinyurl.com/contentful-circleci-webhooks
+
+# Copy static site
+CWD=`pwd`
+
+# Clone Pages repository
+cd /tmp
+git clone git@github.com:richardwestenra/gatsby-contentful-static-test.git build
+# cd build && git checkout -b YOUR_BRANCH origin/YOUR_BRANCH # If not using master
+
+# Trigger Gatsby rebuild
+cd $CWD
+npm run build
+
+# Push newly built repository
+cp -r $CWD/public/* /tmp/build 
+
+cd /tmp/build
+
+# git config --global user.email "rjwestenra@gmail.com"
+# git config --global user.name "Richard Westenra"
+
+git add .
+git commit -m "Automated build"
+git push -f origin master
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby with Contentful`,
+    title: 'Gatsby with Contentful',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
     {
-      resolve: `gatsby-source-contentful`,
+      resolve: 'gatsby-source-contentful',
       options: {
-        spaceId: `uj7lky3by3ba`,
-        accessToken: `59eb0ae6fcfade2242138019487c174dec444fc87d4c9c46931f9b7710949cfe`,
-      },
+        spaceId: 'uj7lky3by3ba',
+        accessToken: '59eb0ae6fcfade2242138019487c174dec444fc87d4c9c46931f9b7710949cfe'
+      }
     },
-    `gatsby-transformer-remark`,
-  ],
+    'gatsby-transformer-remark'
+  ]
 }
