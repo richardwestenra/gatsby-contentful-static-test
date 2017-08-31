e8d3a1b91ea4a9b36c5649902eac85bdf80360e9
richardwestenra <rjwestenra@gmail.com>
2017-08-31 11:14:27 +0000

    Automated build

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+# via https://tinyurl.com/contentful-circleci-webhooks
+
+# show verbose log output
+set -x
+
+# Copy static site
+CWD=`pwd`
+
+# Clone Pages repository
+cd /tmp
+git clone https://github.com/richardwestenra/gatsby-contentful-static-test.git build
+cd build
+git checkout -b gh-pages origin/gh-pages
+
+# Trigger Gatsby rebuild
+cd $CWD
+npm run build
+
+# Push newly built repository
+cp -r $CWD/public/* /tmp/build 
+
+cd /tmp/build
+
+git config --global user.email "$GH_EMAIL"
+git config --global user.name "$GH_NAME"
+
+git add .
+git commit -m "Automated build"
+git push -f origin gh-pages
---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby with Contentful',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'uj7lky3by3ba',
+        accessToken: '59eb0ae6fcfade2242138019487c174dec444fc87d4c9c46931f9b7710949cfe'
+      }
+    },
+    'gatsby-transformer-remark'
+  ]
+}
